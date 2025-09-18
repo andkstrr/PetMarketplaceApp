52bfd00c55ce47f9413d9d7676528d400056c81d
andkstrr <andikasatrionurcahyo@gmail.com>
2025-09-18 14:11:57 +0700

    create welcome page

---
+++ lib/welcome.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    backgroundColor: const Color.fromARGB(255, 70, 120, 193),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(
+            // flex: 2,
+            child: Center(
+              child: Container(
+                height: double.infinity,
+                width: 480,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/welcome.png"),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              )
+            ),
+          ),
+          Expanded(
+            // flex: 1,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              height: 400,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(35),
+                  topRight: Radius.circular(35)
+                )
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 65, vertical: 20),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Find your best friend with us",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.poppins(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w700  
+                          ),
+                        ),
+                        const SizedBox(height: 25),
+                        Text(
+                          "Join us and discover the best pet in your location",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.poppins(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black87,
+                      padding: EdgeInsets.symmetric(horizontal: 120, vertical: 18),
+                    ),
+                    child: Text(
+                      "Get Started",
+                      style: GoogleFonts.poppins(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
